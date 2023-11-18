b90c151385cab5dac8392846387e54b7bcd2e5e0
surajpahari <surajpahari152@gmail.com>
2023-11-18 12:41:01 +0545

    created banner skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Banner } from './components/Banner'
 import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <div className='h-[50px]'>
+        <Navbar />
+      </div>
+      <div className='absolute w-full top-[50px] left-0'>
+        <Banner />
+      </div>
 
     </>
   )
---
+++ frontend/src/components/Banner.tsx
@@ -0,0 +1,34 @@
+export const Banner = () => {
+  return (
+    <div>
+      <div className="h-[500px] w-full bg-green-500">
+        <div className="flex w-full h-full items-center ">
+          <div className="flex-col ml-14 mt-20">
+            <div className="p-2">
+              <div className="text-3xl font-semibold">
+                Moksha
+              </div>
+            </div>
+            <div className="p-2">
+              <div className="text-3xl italic font-medium">
+                Location Matters !
+              </div>
+            </div>
+            <div className="text-xs text-gray-500 p-4">
+              Equiping Humankind with geo-intelligence Solution to make Informed Decisions
+            </div>
+            <div className="mt-20">
+              <div className="flex">
+                <div className="bg-red-400 p-2 rounded text-white hover:bg-orange-500 cursor-pointer">
+                  Get Started with Moksha
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
